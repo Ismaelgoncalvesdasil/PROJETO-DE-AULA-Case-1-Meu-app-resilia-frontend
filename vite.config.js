acf8ef360d68f8325e09d3957ede3427b4b27072
ismael <ismaeldj1234567@gmail.com>
2023-02-26 17:16:02 -0300

    front

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/react",
+  base:"/PROJETO-DE-AULA-Case-1-Meu-app-resilia-frontend.",
   plugins: [react()],
 })
