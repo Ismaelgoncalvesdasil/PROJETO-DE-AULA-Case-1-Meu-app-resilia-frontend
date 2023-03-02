43e1d2cae33bc7e368869a0d8d835d66c4f66584
Ismael Gonçalves da Silva <ismaelgj123567@gmail.com>
2023-03-02 14:56:46 -0300

    dist 3.0

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base:"/PROJETO-DE-AULA-Case-1-Meu-app-resilia-frontend",
-  plugins: [react()],
-})
